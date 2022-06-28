896a5efb1fb0df333a8800aeef3aa04cdc8f6e29
Le Nguyen Khanh Nam <72601429+maphum@users.noreply.github.com>
2022-06-28 21:22:27 +0700

    Update src.js

--- src.js
+++ src.js
@@ -6,7 +6,6 @@ let id0 = document.getElementById("#0")
 let id1 = document.getElementById("#1")
 let id2 = document.getElementById("#2")
 let id3 = document.getElementById("#3")
-
 let kaizen1 = Math.pow(1.0125, odd).toFixed(2);
 let kaizen = Math.pow(1.05, odd);
 let CFproblem = `Do ${(kaizen).toFixed(2)} CF problems ${(1000 * kaizen1)} rating`
